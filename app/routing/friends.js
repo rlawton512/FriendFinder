dc0d1c5b281890a367508e525d2793331d6f219c
rlawton512 <rocsi54321@gmail.com>
2017-08-24 15:36:59 -0400

    finalSubv.1

--- app/routing/friends.js
+++ app/routing/friends.js
@@ -3,9 +3,18 @@ var friendList =
 [
     {
         name: "Victor Riley",
-        photo: "http://pm1.narvii.com/5866/45cc61eb9e3eaefdc26be045e9f9fbfdf916f45b_hq.jpg",
+        photo: "https://s-media-cache-ak0.pinimg.com/originals/ed/43/14/ed431462867f4aaff48fd1790ef23edc.jpg",
         scores: [ 5, 1, 3, 5, 2, 1, 2, 3, 2, 4]
+    }, {
+        name: "Francis Emory",
+        photo: "http://ofad.org/files/daily-photo/recent-portraits-random-people-in-random-places_3.jpg",
+        scores: [1, 3, 4, 5, 2, 5, 1, 2, 3, 4]
+    },{
+        name: "Pamela Hicks",
+        photo: "http://3.bp.blogspot.com/-MfCFEUoPBmQ/UP-TL3WBpOI/AAAAAAAAA-Y/-L9saQrA8tU/s1600/Standard3.jpg",
+        score: [4, 3, 5, 2, 1, 2, 3, 4, 5, 2]
     }
+
 
 ]
 
