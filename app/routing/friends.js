9ac3271a5c2f7aa4fd608896b54fb38ff8dac3cf
rlawton512 <rocsi54321@gmail.com>
2017-08-25 22:32:37 -0400

    finalSubv.3

--- app/routing/friends.js
+++ app/routing/friends.js
@@ -4,7 +4,7 @@ var friendList =
     {
         name: "Victor Riley",
         photo: "https://s-media-cache-ak0.pinimg.com/originals/ed/43/14/ed431462867f4aaff48fd1790ef23edc.jpg",
-        scores: [ 5, 1, 3, 5, 2, 1, 2, 3, 2, 4]
+        scores: [5, 1, 3, 5, 2, 1, 2, 3, 2, 4]
     }, {
         name: "Francis Emory",
         photo: "http://ofad.org/files/daily-photo/recent-portraits-random-people-in-random-places_3.jpg",
@@ -12,7 +12,7 @@ var friendList =
     },{
         name: "Pamela Hicks",
         photo: "http://3.bp.blogspot.com/-MfCFEUoPBmQ/UP-TL3WBpOI/AAAAAAAAA-Y/-L9saQrA8tU/s1600/Standard3.jpg",
-        score: [4, 3, 5, 2, 1, 2, 3, 4, 5, 2]
+        scores: [4, 3, 5, 2, 1, 2, 3, 4, 5, 2]
     }
 
 
