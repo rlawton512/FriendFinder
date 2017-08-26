02807068f814ff24c14ef7f29acaa267a6789d95
rlawton512 <rocsi54321@gmail.com>
2017-08-25 22:34:49 -0400

    Update friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 [
     {
         name: "Victor Riley",
-        photo: "http://pm1.narvii.com/5866/45cc61eb9e3eaefdc26be045e9f9fbfdf916f45b_hq.jpg",
+        photo: "https://s-media-cache-ak0.pinimg.com/originals/ed/43/14/ed431462867f4aaff48fd1790ef23edc.jpg",
         scores: [ 5, 1, 3, 5, 2, 1, 2, 3, 2, 4]
     },{
         name: "Francis Emory",
@@ -12,15 +12,6 @@
         photo: "http://3.bp.blogspot.com/-MfCFEUoPBmQ/UP-TL3WBpOI/AAAAAAAAA-Y/-L9saQrA8tU/s1600/Standard3.jpg",
         score: [4, 3, 5, 2, 1, 2, 3, 4, 5, 2]
 
-    },{
-        name: "Daniel Listo",
-        photo: "https://s-media-cache-ak0.pinimg.com/originals/ed/43/14/ed431462867f4aaff48fd1790ef23edc.jpg",
-        score: [1, 5, 4, 3, 1, 2, 4, 3, 5, 3]
-
-    },{
-        name: "Dawn Little",
-        photo: "https://s-media-cache-ak0.pinimg.com/736x/1f/ae/ef/1faeef67c132e4b6c91e4f17de6b4e49--face-reference-portrait-reference.jpg",
-        score: [3, 5, 4, 5, 3, 4, 1, 2, 4, 5]
     }
 
 ]
